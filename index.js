a2e38175355e29de21e6ce8f20bfc06d085aa80a
Sherwin Terunez <sherwinterunez@Sherwins-MacBook-Air.local>
2017-02-06 13:03:13 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+
+import React from 'react';
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class Avatar extends Component {
+
+  setAvatarColor() {
+    const userName = this.props.name || '';
+    const name = userName.toUpperCase().split(' ');
+    if (name.length === 1) {
+      this.avatarName = `${name[0].charAt(0)}`;
+    } else if (name.length > 1) {
+      this.avatarName = `${name[0].charAt(0)}${name[1].charAt(0)}`;
+    } else {
+      this.avatarName = '';
+    }
+
+    let sumChars = 0;
+    for(let i = 0; i < userName.length; i++) {
+      sumChars += userName.charCodeAt(i);
+    }
+
+    // inspired by https://github.com/wbinnssmith/react-user-avatar
+    // colors from https://flatuicolors.com/
+    const colors = [
+      '#e67e22', // carrot
+      '#2ecc71', // emerald
+      '#3498db', // peter river
+      '#8e44ad', // wisteria
+      '#e74c3c', // alizarin
+      '#1abc9c', // turquoise
+      '#2c3e50', // midnight blue
+    ];
+
+    this.avatarColor = colors[sumChars % colors.length];
+  }
+
+  renderInitials() {
+    return (
+      <Text style={[defaultStyles.textStyle, this.props.textStyle]}>
+        {this.avatarName}
+      </Text>
+    );
+  }
+  
+  render() {
+
+    if (!this.avatarColor) {
+      this.setAvatarColor();
+    }
+
+    return (
+      <TouchableOpacity
+        disabled={this.props.onPress ? false : true}
+        onPress={() => {
+          const {onPress, ...other} = this.props;
+          this.props.onPress && this.props.onPress(other);
+        }}
+        style={[
+          defaultStyles.avatarStyle,
+          {backgroundColor: this.avatarColor},
+          this.props.avatarStyle,
+        ]}
+      >
+        {this.renderInitials()}
+      </TouchableOpacity>
+    );
+  }
+
+}
+
+const defaultStyles = {
+  avatarStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  textStyle: {
+    color: '#fff',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+    fontWeight: '100',
+  },
+};
+
+Avatar.defaultProps = {
+  name: null,
+  onPress: null,
+  avatarStyle: {},
+  textStyle: {},
+};
+
+Avatar.propTypes = {
+  onPress: React.PropTypes.func,
+  avatarStyle: Image.propTypes.style,
+  textStyle: Text.propTypes.style,
+};
